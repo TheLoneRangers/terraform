08b0e613fe86c96bc7f763a3f106e004664e7e6c
joshuahargrove <jhargr200@gmail.com>
2021-07-10 13:02:42 -0600

    hmm

--- terraform/main.tf
+++ terraform/main.tf
@@ -4,10 +4,19 @@ terraform {
       source  = "kreuzwerker/docker"
       version = "~> 2.13.0"
     }
+    proxmox = {
+      source = "Telmate/proxmox"
+      version = "2.7.3"
+    }
   }
 }
 
 provider "docker" {}
+
+#proxmox creds are exposed via environment vars
+provider "proxmox" {
+  pm_api_url = "https://192.168.1.53:8006/api2/json"
+}
 
 resource "docker_image" "nginx" {
   name         = "nginx:latest"
