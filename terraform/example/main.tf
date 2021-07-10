d982cdcea4466aedff5dbd2b413e7203b4071ad1
joshuahargrove <jhargr200@gmail.com>
2021-07-10 14:45:55 -0600

    lets see

---
+++ terraform/example/main.tf
@@ -0,0 +1,58 @@
+terraform {
+  required_providers {
+    proxmox = {
+      source = "Telmate/proxmox"
+      version = "2.7.3"
+    }
+  }
+}
+
+#proxmox creds are exposed via environment vars
+provider "proxmox" {
+  pm_api_url = "https://192.168.1.53:8006/api2/json"
+  pm_tls_insecure = true
+}
+
+resource "proxmox_vm_qemu" "cloudinit-test" {
+  name = "terraform-vm-test"
+  desc = "test to see if this works"
+
+  target_node = "Datacenter"
+
+  pool = "vm1"
+
+  clone = "VM 9000"
+
+  agent = 1
+
+  os_type = "cloud-init"
+  cores = 2
+  sockets = 1
+  vcpus = 0
+  cpu = "host"
+  memory = 2048
+  scsihw =  "lsi"
+
+  disk {
+    size = 10
+    type = "virtio"
+    storage = "ceph-storage-pool"
+    storage_type = "rbd"
+    iothread = 1
+    ssd = 1
+    discard = "on"
+  }
+
+  network {
+    model = "virtio"
+    bridge = "vmbr0"
+    tag = 256
+  }
+
+  ipconfig0 = "ip=192.168.1.90/24,gw=192.168.0.1"
+
+  sshkeys = <<EOF
+  ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMa9BkreshcNcL0bVKR5xEtH/JoGOv/h3rEFF90FuCl1 jhargr200@gmail.com
+  EOF
+
+}
