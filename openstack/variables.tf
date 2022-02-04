948789c1b4948fda47f04c22db5c5ed6fd5a64ad
joshuahargrove <jhargr200@gmail.com>
2022-02-04 16:15:24 -0700

    oops, gonna need these.

---
+++ openstack/variables.tf
@@ -0,0 +1,11 @@
+variable "os_username" {
+  description = "OS username"
+  type        = string
+  sensitive   = true
+}
+
+variable "os_password" {
+  description = "OS password"
+  type        = string
+  sensitive   = true
+}
